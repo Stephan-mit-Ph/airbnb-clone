339bbae290f1481c2f4422d81acf02af287c822a
Stephan Niendorf <stephan@stephanmitph.com>
2023-06-25 01:10:22 +0200

    add infocards

--- app/search/page.js
+++ app/search/page.js
@@ -1,3 +1,12 @@
-export default function Search() {
-  return <h1>My Page</h1>;
+import { ResultsList } from "./results/ResultsList";
+
+export default async function Search() {
+  const res = await fetch("http://localhost:3000/api/search");
+  const data = await res.json();
+
+  return (
+    <>
+      <ResultsList data={data} />
+    </>
+  );
 }
