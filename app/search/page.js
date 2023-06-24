f95d6fb5c5061464819d1341fc71c72115b4a7e9
Stephan Niendorf <stephan@stephanmitph.com>
2023-06-25 01:18:22 +0200

    set api path

--- app/search/page.js
+++ app/search/page.js
@@ -1,7 +1,9 @@
 import { ResultsList } from "./results/ResultsList";
 
 export default async function Search() {
-  const res = await fetch("/api/search");
+  const res = await fetch(
+    "https://airbnb-clone-seven-flame-vercel.app/api/search"
+  );
   const data = await res.json();
 
   return (
