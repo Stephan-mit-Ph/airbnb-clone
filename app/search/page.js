cf98af29f81aff0a03c3d1dc42f45c5caa170ffe
Stephan Niendorf <stephan@stephanmitph.com>
2023-06-25 01:15:21 +0200

    fix path

--- app/search/page.js
+++ app/search/page.js
@@ -1,7 +1,7 @@
 import { ResultsList } from "./results/ResultsList";
 
 export default async function Search() {
-  const res = await fetch("http://localhost:3000/api/search");
+  const res = await fetch("/api/search");
   const data = await res.json();
 
   return (
