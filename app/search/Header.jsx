866a1477292e6e716d3febc8fbc280c99d60d3aa
Stephan Niendorf <stephan@stephanmitph.com>
2023-06-25 04:27:58 +0200

    add functionality

--- app/search/Header.jsx
+++ app/search/Header.jsx
@@ -6,19 +6,24 @@ import Image from "next/image.js";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { useState, useRef } from "react";
 import { useClickAway } from "react-use";
+import { motion } from "framer-motion";
+import { useSearchStore, initialState } from "@/store";
 
 import SearchBar from "./components/SearchBar.jsx";
 
 export default function Header() {
   const [isExpanded, setIsExpanded] = useState(false);
   const ref = useRef(null);
+  const searchStore = useSearchStore((state) => state);
 
   const toggleExpanded = () => {
     setIsExpanded((prevIsExpanded) => !prevIsExpanded);
   };
 
   useClickAway(ref, () => {
-    if (isExpanded) setIsExpanded(false);
+    if (isExpanded) {
+      setIsExpanded(false);
+    }
   });
 
   const headerContainerClasses = clsx(
@@ -27,8 +32,8 @@ export default function Header() {
     "flex",
     "justify-between",
     "bg-white",
-    "z-50",
     "py-8",
+    "z-50",
     {
       "h-[7.5rem]": !isExpanded,
       "h-[13rem]": isExpanded,
@@ -46,6 +51,8 @@ export default function Header() {
     "border",
     "drop-shadow-md",
     "bg-white",
+    "w-auto",
+    "self-center",
     {
       "border-b-0": !isExpanded,
       "border-b-8": isExpanded,
@@ -70,32 +77,101 @@ export default function Header() {
     }
   );
 
+  const userIconClasses = clsx("text-slate-600 flex", {
+    "items-center": !isExpanded,
+    "items-start": isExpanded,
+  });
+
+  const searchContainerVariants = {
+    initial: {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+    },
+    hidden: {
+      opacity: 0,
+      y: 100,
+      scale: 2,
+    },
+    enter: {
+      opacity: 1,
+      y: 0,
+      scale: 1,
+    },
+  };
+
+  const tabVariants = {
+    hidden: {
+      opacity: 0,
+      height: 0,
+      y: -100,
+      scale: 0,
+    },
+    enter: {
+      opacity: 1,
+      height: "auto",
+      y: 0,
+      scale: 1,
+    },
+    exit: {
+      opacity: 0,
+      height: 0,
+      y: -100,
+      scale: 0,
+    },
+  };
+
   return (
     <>
       <header ref={ref} className="flex border-b bg-white z-50 fixed w-full">
         <div className={headerContainerClasses}>
           <div className="text-red-500">
-            <Image src="/images/logo.png" alt="logo" width={172} height={50} />
+            <Image src="/images/logo.png" height={50} width={172} alt="Logo" />
           </div>
-          {isExpanded ? (
-            <SearchBar />
-          ) : (
-            <button onClick={toggleExpanded} className={searchContainerClasses}>
-              <div className="input bg-inherit flex items-center border-r px-4">
-                <p>Anywhere</p>
+          <div className="flex flex-col grow">
+            <motion.div
+              className="flex flex-col justify-center"
+              variants={tabVariants}
+              initial="hidden"
+              animate={isExpanded ? "enter" : "initial"}
+              transition={{ type: "linear" }}
+            >
+              <SearchBar toggleExpanded={toggleExpanded} />
+            </motion.div>
+            <motion.button
+              inital="inital"
+              animate={isExpanded ? "hidden" : "inital"}
+              transition={{ type: "linear" }}
+              onClick={toggleExpanded}
+              variants={searchContainerVariants}
+              className={searchContainerClasses}
+            >
+              <div className="input flex items-center border-r px-4">
+                <p>
+                  {searchStore.location !== ""
+                    ? searchStore.location
+                    : "Anywhere"}
+                </p>
               </div>
-              <div className="input bg-inherit flex items-center border-r px-4">
-                <p>Any Date</p>
+              <div className="input flex items-center border-r px-4">
+                <p>
+                  {searchStore.dates[0] !== initialState.dates[0] &&
+                  searchStore.dates[1] !== initialState.dates[1]
+                    ? `${searchStore.dates[0].toDateString()} - ${searchStore.dates[1].toDateString()}`
+                    : "Any Date"}
+                </p>
               </div>
-              <div className="input bg-inherit flex items-center border-r px-4">
-                <p>Add Guests</p>
+              <div className="input flex items-center border-r px-4">
+                <p>
+                  {searchStore.guests > 0 ? searchStore.guests : "Add Guests"}
+                </p>
               </div>
               <div className="search-btn px-4 rounded-full bg-primary h-10 w-10 relative">
                 <MagnifyingGlassIcon className="h-4 w-5 text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
               </div>
-            </button>
-          )}
-          <div>
+            </motion.button>
+          </div>
+          <div className={userIconClasses}>
             <Image src="/images/user.svg" height={30} width={30} alt="User" />
           </div>
         </div>
--- app/search/page.js
+++ app/search/page.js
@@ -1,5 +1,7 @@
 import { ResultsList } from "./results/ResultsList";
 
+import Link from "next/link";
+
 export default async function Search() {
   const res = await fetch(
     "https://airbnb-clone-virid-iota.vercel.app/api/search"
@@ -8,6 +10,14 @@ export default async function Search() {
 
   return (
     <>
+      <div className="flex items-center justify-center w-screen bg-cover bg-center h-48 bg-[url('/images/booking-website-hero.png')]">
+        <Link
+          href="/search/results"
+          className="rounded-full text-white bg-[#673979] px-4 py-2 hover:bg-[#7B3C7D]"
+        >
+          Browse Stays
+        </Link>
+      </div>
       <ResultsList data={data} />
     </>
   );
